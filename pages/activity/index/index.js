54642485642075569a6f9f7232206161637eb8c9
linjunfeng <jeff.lin@halobop.com>
2018-12-18 20:56:32 +0800

    小程序初步代码提交

---
+++ pages/activity/index/index.js
@@ -0,0 +1,90 @@
+// pages/activity/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activity: [
+      {
+        'tmBegin': '12月18号',
+        'week': '星期二',
+        'avatar': '/images/test/avatar.jpg',
+        'title': '冬至吃汤圆报名啦！！！',
+        'logo': '/images/test/logo.jpg'
+      },
+      {
+        'tmBegin': '12月18号',
+        'week': '星期二',
+        'avatar': '/images/test/avatar.jpg',
+        'title': '冬至吃汤圆报名啦！！！冬至吃汤圆报名啦！！！冬至吃汤圆报名啦！！！',
+        'logo': '/images/test/logo.jpg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 自定义方法
+   */
+  goDetail: function () {
+    wx.navigateTo({
+      url: '/pages/activity/detail/detail'
+    })
+  }
+})
---
+++ pages/home/index/index.js
@@ -0,0 +1,89 @@
+// pages/home/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner: [
+      {
+        'title': '那些年，我们一起过的生日',
+        'url': 'https://media.links2all.com/FvmgDf9wVCI_ZUO1SoCI13aiKBsa'
+      },
+      {
+        'title': '冬至在食堂的我们',
+        'url': 'https://media.links2all.com/FlIhl2L_uB8yBBtTniuE2nJ--U09'
+      },
+      {
+        'title': '那些年，我们一起过的生日',
+        'url': 'https://media.links2all.com/FvmgDf9wVCI_ZUO1SoCI13aiKBsa'
+      },
+      {
+        'title': '冬至在食堂的我们',
+        'url': 'https://media.links2all.com/FlIhl2L_uB8yBBtTniuE2nJ--U09'
+      }
+    ],
+    articles: [
+      {
+        'title': '震惊！惠大侨中人今年冬至过如过派......',
+        'avatar': 'https://media.links2all.com/FlIhl2L_uB8yBBtTniuE2nJ--U09'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
